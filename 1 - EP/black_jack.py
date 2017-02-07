7e0e34359ac89796a97c74019f979d95648fb046
Padawan <Padawan>
2017-02-07 19:33:01 -0200

    second commit

--- 1 - EP/black_jack.py
+++ 1 - EP/black_jack.py
@@ -1,5 +1,49 @@
 from random import randint
 
+
+playerHand = []
+dealerHand = []
+
+def addCardHand(n, player):
+
+    if(player):
+        if(Card in playerHand):
+            for i in range(0,n):
+                playerHand.append(Card)
+        elif
+            giveCard()
+    elif(dealer):
+        if(Card in playerHand):
+            for i in range(0,n):
+                dealerHand.append(Card)
+        elif
+            giveCard()
+            
+def giveCard():
+
+    
+    NumberCard = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+    NipeCard = ["C","P","O","E"]
+
+    Number = NumberCad[randint(0,12)]
+    Nipe = NipeCard[randint(0,3)]
+
+    Card = (Number, Nipe)
+
+    return Card
+
+
+def newRound():
+    
+    choice = input("\nDeseja jogar mais uma (S) ou (N)?\n")
+
+    if choice == 'S' or choice == 's':
+        game_loop()
+    if choice == 'N' or choice == 'n':
+        return
+        is_running = False 
+
+
 def NumberCard():
     
     numVector = [1,2,3,4,5,6,7,8,9,10,11,12,13]
@@ -10,6 +54,7 @@ def NumberCard():
     #Guarda o numero aleatorio da carta em cardNumber
 
     return (int(cardNumber))
+    #Retorna em inteiro o valor da carta
 
 def NipeCard():
         
@@ -21,98 +66,202 @@ def NipeCard():
     #Guarda o nipe gerado em cardNipe 
 
     return (str(cardNipe))
+    #Retorna como string o digito do nipe
+
 
 def game_loop():
     is_running = True
     while is_running:
-        user_choice = input('O que voce quer? (N)umero ou (S)air \n')
+        user_choice = input('\n\nO que voce quer? (N)umero ou (S)air \n')
         if user_choice == 'N' or user_choice == 'n':
      
             print("\n ===================BLACK JACK===================\n")
-            
-            iconVector = ['A',2,3,4,5,6,7,8,9,10,'J','Q','K'] #0,12
-            deckPlayerNumber = []
-            deckPlayerNipe = []
-            deckDealerNumber = []
-            deckDealerNipe = []
+    
+            iconVector = ['A',2,3,4,5,6,7,8,9,10,'J','Q','K'] #
+            #Vetor que referencia o numero randomizado com o valor impresso da carta
+            calcVector = [1,2,3,4,5,6,7,8,9,10,10,10,10]
+
+            handPlayerNumber = [];
+            #Numero das cartas que estão na mão do Player
+            handPlayerNipe = []
+            #Nipe das cartas que estão na mão do Player
+            handDealerNumber = []
+            #Numero das cartas que estão na mão do Dealer
+            handDealerNipe = []
+            #Nipe das cartas que estão na mão do Dealer
+            handDealerVisible = False
+
 
             #Primeira Rodada
             rodada = 1
             if(rodada == 1):
                 cartas = 0
                 while cartas < 2:
-                    deckPlayerNumber.append(NumberCard())
-                    deckPlayerNipe.append(NipeCard())
-                    deckDealerNumber.append(NumberCard())
-                    deckDealerNipe.append(NipeCard())
+                    handPlayerNumber.append(NumberCard())
+                    handPlayerNipe.append(NipeCard())
+                    handDealerNumber.append(NumberCard())
+                    handDealerNipe.append(NipeCard())
                     cartas += 1
             
                                     
-            print ('YOU \t',str(iconVector[deckPlayerNumber[0] - 1]) + deckPlayerNipe[0], str(iconVector[deckPlayerNumber[1] - 1]) + deckPlayerNipe[1])
+            print ('YOU \t',str(iconVector[handPlayerNumber[0] - 1]) + handPlayerNipe[0], '|', str(iconVector[handPlayerNumber[1] - 1]) + handPlayerNipe[1])
             
-            print ('DEALER \t',str(iconVector[deckDealerNumber[0] - 1]) + deckDealerNipe[0], '?')
+            print ('DEALER \t',str(iconVector[handDealerNumber[0] - 1]) + handDealerNipe[0], '|', '?')
 
 
-            if(deckPlayerNumber[0] == 1):
-                if(deckPlayerNumber[1] == 11 or deckPlayerNumber[1] == 12 or deckPlayerNumber[1] == 13):
-
-                    print("\n *****$$$$****21*****$$$$*****\n")
+            if(handPlayerNumber[0] == 1):
+                if(handPlayerNumber[1] == 11 or handPlayerNumber[1] == 12 or handPlayerNumber[1] == 13):
+                    handPlayerNumber[0] = 11
                     print("\n *****$$$$*********PLAYER WINS********$$$$*****\n")
-                    
+                    break
 
-            if(deckDealerNumber[0] == 11 or deckDealerNumber[0] == 12 or deckDealerNumber[0] == 13):
-                if(deckDealerNumber[1] == 1):
-                    print ('DEALER \t\n',str(iconVector[deckDealerNumber[0] - 1]) + deckDealerNipe[0], str(iconVector[deckDealerNumber[1] - 1]) + deckDealerNipe[1])
-                    
-                    print("\n *****$$$$****21*****$$$$*****\n")
+            if(handDealerNumber[0] == 11 or handDealerNumber[0] == 12 or handDealerNumber[0] == 13):
+                if(handDealerNumber[1] == 1):
+                    print ('\nDEALER \t',str(iconVector[handDealerNumber[0] - 1]) + handDealerNipe[0], str(iconVector[handDealerNumber[1] - 1]) + handDealerNipe[1])
+                    handDealerNumber[0] = 11
+
                     print("\n *****$$$$*********DEALER WINS********$$$$*****\n")
+                    break
+                            
+
+            limitPlayer = calcVector[handPlayerNumber[0] - 1] + calcVector[handPlayerNumber[1] - 1]
+            limitDealer = calcVector[handDealerNumber[0] - 1] + calcVector[handDealerNumber[1] - 1]
             
-            rodada = 2
+            print("p",limitPlayer)
+            print("d",limitDealer)
+                        
+            
+            if limitPlayer > 21:
+                print('\n$-DEALER -> WINS-$', "\n", 'Dealer =', limitDealer,'Player =', limitPlayer)
+                return
+            elif limitPlayer == 21:
+                print('\n$-PLAYER -> WINS -$', "\n", 'Dealer =', limitDealer,'Player =', limitPlayer)
+                return   
+            elif limitDealer > 21:
+                print('\nPLAYER -> WINS -$', "\n", 'Dealer =', limitDealer,'Player =', limitPlayer)
+                return
+            elif limitDealer == 21:
+                print('\n$-DEALER -> WINS-$', "\n", 'Dealer =', limitDealer,'Player =', limitPlayer)
+                return
 
-            limitPlayer = 0
-            limitDealer = 0
+            for i in range (2,21):
+                hit_stand = input('\n(H)it ou (S)tand\n')
 
-            for i in range (0,21):
-                hit_stand = input('(H)it ou (S)tand\n')
-                if hit_stand == 'H' or hit_stand == 'h':
-                    
-                    deckPlayerNumber.append(NumberCard())
-                    deckPlayerNipe.append(NipeCard())
-                    deckDealerNumber.append(NumberCard())
-                    deckDealerNipe.append(NipeCard())
-                    
-                    limitPlayer = limitPlayer + deckPlayerNumber[i]
-                    limitDealer = limitDealer + deckDealerNumber[i]
+                if hit_stand == '\nH' or hit_stand == 'h':
+                        
+                    handPlayerNumber.append(NumberCard())
+                    handPlayerNipe.append(NipeCard())
 
-                    print('Player Hand -> \t', iconVector[deckPlayerNumber[i] - 1],  deckPlayerNipe[i])
-                    print('Dealer Hand -> \t', iconVector[deckDealerNumber[0] - 1],  deckDealerNipe[0])
-
-                    print('\nSoma Player', limitPlayer)
-                    print('\nSoma Dealer', limitDealer)
+                    l = 0
+                    while(l < len(handPlayerNumber)):
+                        if(l == 0):
+                            print ('\nYOU \t', end = " ")
+                        print(iconVector[handPlayerNumber[l] - 1], handPlayerNipe[l], '|', end = " ")
+                        l += 1
+                        
+                    limitPlayer = limitPlayer + calcVector[handPlayerNumber[i] - 1]
 
                     if limitPlayer > 21:
-                        print('PLAYER -> LOSE -$- DEALER -> WINS \.0./ ')
+                        print('\n$-DEALER -> WINS-$', "\n", 'Dealer =', limitDealer,'Player =', limitPlayer)
+                        handDealerVisible = True
+                        return
                     elif limitPlayer == 21:
-                        print('PLAYER -> WINS -$- DEALER -> LOSE \.0./ ')    
+                        print('\n$-PLAYER -> WINS -$', "\n", 'Dealer =', limitDealer,'Player =', limitPlayer)
+                        handDealerVisible = True
+                        return    
                     elif limitDealer > 21:
-                        print('PLAYER -> WINS -$- DEALER -> LOSE \.0./ ')
+                        print('\nPLAYER -> WINS -$', "\n", 'Dealer =', limitDealer,'Player =', limitPlayer)
+                        handDealerVisible = True
+                        return
                     elif limitDealer == 21:
-                        print('PLAYER -> LOSE -$- DEALER -> WINS \.0./ ')
-             
+                        print('\n$-DEALER -> WINS-$', "\n", 'Dealer =', limitDealer,'Player =', limitPlayer)
+                        handDealerVisible = True
+                        return
+                    if limitDealer > 21 and limitPlayer > 21:
+                        if(limitDealer == limitPlayer):
+                            print('!!!EMPATE!!! - DEALER WINS')
+                            handDealerVisible = True
+                            return
+
+                    if(handDealerVisible == True):
+                        d = 0
+                        while(d < len(handDealerNumber)):
+                            if(d == 0):
+                                print('Dealer \t', end = " ")
+                            print(iconVector[handDealerNumber[d] - 1], handDealerNipe[d], "|", end = " ")
+                            d+=1
+                            handDealerVisible = False
+                    else:
+                        print('\nDEALER \t', end = " ")
+                        print(iconVector[handDealerNumber[0] - 1], handDealerNipe[0], '|', "?", "|", "\n")   
+                    
+
                 if hit_stand == 'S' or hit_stand == 's':
-                    j = 0
-                    for j in range(0,i):
-                        print(iconVector[deckDealerNumber[j] - 1], deckDealerNipe[j], '|', end = " ")
-                        j = j + 1 
-                        print(iconVector[deckDealerNumber[j] - 1], deckDealerNipe[j], '|', end = " ")
-                        j = j + 1
-                        print(iconVector[deckDealerNumber[j] - 1], deckDealerNipe[j])
+                #----- STAND -------#
+    
+                    limitDealerDif = abs(limitDealer - 21)
+                    limitPlayerDif = abs(limitPlayer - 21)          
+
+                    k = 0
+                    while(k < len(handDealerNumber)):
+                        if(k == 0):
+                            print ('DEALER \t', end = " ")
+                        print(iconVector[handDealerNumber[k] - 1], handDealerNipe[k], '|', end = " ")
+                        k += 1
+
+                    if(limitDealer >= 16 and limitDealer < 21):
+                        if limitDealerDif < limitPlayerDif:
+                            print('\n$-DEALER -> WINS-$', "\n", 'Dealer - 21 =', limitDealerDif,'Player - 21 =',limitPlayerDif)
+                            return
+                        elif limitDealerDif > limitPlayerDif:
+                            print('\n$-PLAYER -> WINS-$', "\n", 'Dealer - 21 =', limitDealerDif,'Player - 21 =',limitPlayerDif)
+                            return  
                     
-            i += 1
-                    
+                    p = 2
+                    while(limitDealer <= 16):
+                                           
+                        handDealerNumber.append(NumberCard())
+                        handDealerNipe.append(NipeCard())
+
+                        f = 0
+                        while(f < len(handDealerNumber)):
+                            if(f == 0):
+                                print('\nDEALER \t', end = " ")
+                            print(iconVector[handDealerNumber[f] - 1], handDealerNipe[f], '|', end = " ")
+                            f += 1
+
+                        limitDealer = limitDealer + calcVector[handDealerNumber[p] - 1]
+
+                        limitDealerDif = abs(limitDealer - 21)
+                        limitPlayerDif = abs(limitPlayer - 21)          
+ 
+    
+                        if(limitDealer > 16 and limitDealer < 21):
+                            if limitDealerDif < limitPlayerDif:
+                                print('\n$-DEALER -> WINS-$', "\n", 'Dealer - 21 =', limitDealerDif,'Player - 21 =',limitPlayerDif)
+                                return
+                            elif limitDealerDif > limitPlayerDif:
+                                print('\n$-PLAYER -> WINS-$', "\n", 'Dealer - 21 =', limitDealerDif,'Player - 21 =',limitPlayerDif)
+                                return
+                        if limitDealer > 21:
+                            print('\nPLAYER -> WINS -$', "\n", 'Dealer =', limitDealer,'Player =', limitPlayer)
+                            return
+                        elif limitDealer == 21:
+                            print('\n$-DEALER -> WINS-$', "\n", 'Dealer =', limitDealer,'Player =', limitPlayer)
+                            return
+                        if limitDealer > 21 and limitPlayer > 21:
+                            if(limitDealer == limitPlayer):
+                                print('!!!EMPATE!!! - DEALER WINS')
+
+                            return
+                   
+                        p += 1
+                
+            #for inicial
+
         elif user_choice == 'S' or user_choice == 's':
             print('Tchau!')
             is_running = False
 
 if __name__ == "__main__":
-    game_loop()
+    newRound()
---
+++ 1 - EP/black_jack2.0.py
@@ -0,0 +1,201 @@
+from random import randint
+
+playerHand = []
+dealerHand = []
+
+def dif(totalPlayer,totalDealer):
+
+    dealerVisible = True
+    difPlayer = abs(totalPlayer - 21)
+    difDealer = abs(totalDealer - 21)
+
+    if(difDealer < difPlayer):        
+        showCards('dealer', dealerVisible)
+        print('\nDIF => Dealer ->', difDealer, 'Player ->', difPlayer)
+        print('\nGAME OVER -- 21 -- DEALERWINS')
+        return False
+    elif(difPlayer < difDealer):        
+        showCards('dealer', dealerVisible)
+        print('\nDIF => Dealer ->', difDealer, 'Player ->', difPlayer, "\n")
+        print('\nGAME OVER -- 21 -- PLAYERWINS')
+        return False
+    else:
+        return True
+
+def twentyOne(totalPlayer,totalDealer):
+
+    dealerVisible = False
+    difDealer = abs(totalDealer - 21)
+    difPlayer = abs(totalPlayer - 21)
+
+    if totalPlayer == 21 or totalDealer > 21:
+        print('\nTOTAL => Dealer ->', totalDealer,'Player ->', totalPlayer)
+        print('\nGAME OVER -- 21 -- PLAYERWINS')
+        return False
+
+    elif totalDealer == 21 or totalPlayer > 21:
+        print('\nTOTAL => Dealer ->', totalDealer,'Player ->', totalPlayer)
+        print('\nGAME OVER -- 21 -- DEALERWINS')
+        return False
+
+    elif totalDealer == 21 and totalPlayer == 21:
+        print('\nTOTAL => Dealer ->', totalDealer,'Player ->', totalPlayer)
+        print('\nGAME OVER -- 21 -- DEALERWINS')
+        return False
+
+    else:
+        return True
+
+def HitStand():
+
+    dealerVisible = False
+
+    Hit = True
+
+    while(Hit == True):
+        user_choice = input("\n(H)IT OU (S)TAND ?\t")
+        if(user_choice == 'H' or user_choice == 'h'):
+            addCardHand(1,'player')
+            s = sumCards()
+            showCards('ambos', dealerVisible)
+            Hit = twentyOne(s[0], s[1])
+
+        elif(user_choice == 'S' or user_choice == 's'):
+            Hit = False
+            dealerVisible = True
+            dealerS = sumCards()
+            while(dealerS[1] < 16): 
+                addCardHand(1,'dealer')
+                dealerS = sumCards()
+                showCards('dealer', dealerVisible)
+            if(dealerS[1] >= 21):
+                twentyOne(dealerS[0],dealerS[1])
+            elif(dealerS[1] < 21):
+                dif(dealerS[0],dealerS[1])
+
+def showCards(p, dealerVisible):
+
+    exibitionCards = ['A',2,3,4,5,6,7,8,9,10,'J','Q','K']
+
+    if(p == "player"):
+        for i in range(len(playerHand)):
+            if (i == 0):
+                print('\nYOU ->', end = " ")
+            print(exibitionCards[playerHand[i][0] - 1], playerHand[i][1], "|", end = " ")
+
+    if(p == "dealer"):
+        print('\nDEALER ->', end = " ") 
+        for j in range(len(dealerHand)):
+            if(dealerVisible == True):
+                print(exibitionCards[dealerHand[j][0] - 1], dealerHand[j][1], "|", end = " ")
+            elif(j <= 0 and dealerVisible == False):
+                print(exibitionCards[dealerHand[0][0] - 1], dealerHand[0][1], "|", "?\n") 
+
+    if(p == "ambos"):
+        for k in range(len(playerHand)):
+            if (k == 0):
+                print('\nYOU ->', end = " ")
+            print(exibitionCards[playerHand[k][0] - 1], playerHand[k][1], "|", end = " ")
+
+        for y in range(len(dealerHand)):
+            if(y == 0):
+                print('\nDEALER ->', end = " ")
+                if(dealerVisible == True):
+                    print(exibitionCards[dealerHand[y][0] - 1], dealerHand[y][1], "|", end = " ")
+                elif(y < 2 and dealerVisible == False):
+                    print(exibitionCards[dealerHand[0][0] - 1], dealerHand[0][1], "|", "?")
+                    return dealerVisible
+
+def sumCards():
+
+    valorCards = [1,2,3,4,5,6,7,8,9,10,10,10,10]
+
+    totalDealer = 0
+    totalPlayer = 0
+
+    for i in range(len(playerHand)):
+       totalPlayer += valorCards[playerHand[i][0] - 1]
+
+    for j in range(len(dealerHand)):
+        totalDealer += valorCards[dealerHand[j][0] - 1]
+
+    return totalPlayer,totalDealer
+
+def firstRound():
+
+    dealerVisible = False
+
+    addCardHand(2,'player')
+    addCardHand(2,'dealer')
+
+    showCards('player', dealerVisible)
+    showCards('dealer', dealerVisible)
+
+    JQK= [11,12,13]
+
+    for i in range(0,2):
+        for j in range(0,2):
+            if(playerHand[i][0] == 1 and playerHand[j][0] in JQK):
+                print("\n*********FIRST HIT WIN*******\n")
+                print("\nGAME OVER -- 21 -- PLAYER-WINS\n")
+                game_loop()
+            elif(dealerHand[i][0] == 1 and dealerHand[j][0] in JQK):
+                dealerVisible = True 
+                print("\n*********FIRST HIT WIN*******\n")
+                showCards('dealer', dealerVisible)
+                print("\nGAME OVER -- 21 -- DEALER-WINS\n")
+                game_loop()
+
+def playGame():
+
+    print('-------------------------WELCOME TO-------------------------\n')
+    print('-------------------------BLACK JACK-------------------------\n')
+    playerHand.clear()
+    dealerHand.clear()
+
+    firstRound()
+    HitStand()
+
+def addCardHand(n,p):
+
+    Card = giveCard()
+
+    if(p == 'player'):
+        for i in range(0, n):
+            while(Card in playerHand):
+                Card = giveCard()
+            if((Card in playerHand) == False and (Card in dealerHand) == False):
+                playerHand.append(Card)
+    if(p == 'dealer'):
+        for j in range(0, n):
+            while(Card in dealerHand):
+                Card = giveCard()
+            if((Card in dealerHand) == False and (Card in dealerHand) == False):
+                dealerHand.append(Card)
+
+def giveCard():
+
+    NumberCard = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+    NipeCard = ["C","P","O","E"]
+
+    Number = NumberCard[randint(0,12)]
+    Nipe = NipeCard[randint(0,3)]
+
+    Card = (Number, Nipe)
+
+    return Card
+
+def game_loop():
+    is_running = True
+    while is_running:
+        user_choice = input('\nLet.s Play? (S)im - (N)ão \n')
+        if user_choice == 'S' or user_choice == 's':
+
+           playGame()
+
+        elif user_choice == 'N' or user_choice == 'n':
+            print('Tchau!')
+            is_running = False
+
+if __name__ == "__main__":
+    game_loop()
